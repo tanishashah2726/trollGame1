115dfeae5fb249560e001381b1ef33eefa5e3696
tanishashah2726 <tanishashah1026@gmail.com>
2026-09-22 22:06:36 -0400

    added a way to switch through the levels by entering a "door"

--- scripts/door.gd
+++ scripts/door.gd
@@ -2,7 +2,10 @@ extends Area2D
 
 
 
-
-
 func _on_body_entered(body: Node2D) -> void:
-	print("Level 2")
+	var current_scene_file = get_tree().current_scene.scene_file_path
+	var next_level_num = current_scene_file.to_int() +1
+	
+	var next_level_path = "res://scenes/levels/level" + str(next_level_num) + ".tscn"
+	
+	get_tree().call_deferred("change_scene_to_file",next_level_path)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 170.0
+const JUMP_VELOCITY = -320.0
 
 
 func _physics_process(delta: float) -> void:
